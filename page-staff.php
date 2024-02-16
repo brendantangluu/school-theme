8d1397970f406c35ad9801f9a0db70675f8a7695
brendantangluu <btangluu@my.bcit.ca>
2024-02-16 11:37:05 -0800

    staff page is done and styled

--- page-staff.php
+++ page-staff.php
@@ -16,21 +16,67 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+		<div class = 'staff-section'>
+			<header>
+				<h1><?php the_title() ?></h1>
+			</header>
+				<!-- Output the posts -->
+			<?php
+				$taxonomy = 'school-staff-category';
+					$terms = get_terms( 
+						array(
+							'taxonomy' => $taxonomy,
+						) 
+					);
+					if ( $terms && ! is_wp_error( $terms ) ) {
+						foreach ( $terms as $term ) {			
+							$args = array(
+								'post_type'      => 'school-staff',
+								'posts_per_page' => -1,
+								'tax_query'		 => array(
+									array(
+										'taxonomy'	=> 'school-staff-category',
+										'field'		=> 'slug',
+										'terms'  	=> $term->slug
+									)
+								)
+							);
+	
+							$query = new WP_Query($args);
+							if($query -> have_posts()){
+								echo '<section><h2 class = "staff-category">' . esc_html__( $term->name, 'fwd') .'</h2>';
+									while($query -> have_posts()){
+										$query -> the_post();
+										
+										?>
+										<article>
+											<?php
+												$id = get_the_id();
+											?>
+												<h3 id=<?php echo $id; ?>><?php the_title(); ?></h3>
+												<p><?php the_field('staff_biography')?></p>
+												<?php 
+													if(get_field('courses') or get_field('instructor_website')){
+														?> 
+														<p>Courses: <?php the_field('courses') ?></p>
+														<a href="<?php the_field('instructor_website') ?>">Instructor Website</a>
+														<?php
+													}
+												
+												?>
+										</article>
+										<?php
+									}
+								echo '</section>';
+								wp_reset_postdata();
+							}
+						}
+						// Add your WP_Query() code here
+						// Use $term->slug in your tax_query
+						// Use $term->name to organize the posts
+					}
+			?>
+		</div>
 	</main><!-- #main -->
 
 <?php
