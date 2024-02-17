b7b2b7ed79cbb5c7455279d93ebd5687d6cc30d0
brendantangluu <btangluu@my.bcit.ca>
2024-02-16 18:45:40 -0800

    creating the page-class template

--- page-the-class.php
+++ page-the-class.php
@@ -16,23 +16,57 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+		
 		<?php
-		$args = array(
-			'post_type'		=> 'school-student',
-			'posts_per_page'=> -1,
-			'orderby'		=> 'title',
-			'order'			=> 'ASC'
+		get_template_part( 'template-parts/content', 'page' );
+		
+		$taxonomy = 'school-specialty-category';
+		$terms = get_terms(
+			array(
+				'taxonomy' => $taxonomy,
+			)
 		);
+		if($terms && ! is_wp_error($terms)){
+			foreach($terms as $term){
+				$args = array(
+					'post_type'      => 'school-student',
+					'posts_per_page' => -1,
+					'orderby'		 => 'title',
+					'order'			 => 'ASC',
+					'tax_query'		 => array(
+						array(
+							'taxonomy'	=> 'school-specialty-category',
+							'field'		=> 'slug',
+							'terms'  	=> $term->slug
+						)
+					)
+				);
+				$query = new WP_Query($args);
+				if($query -> have_posts()){
+					while($query -> have_posts()){
+						$query -> the_post();
+						
+						?>
+						<article>
+							<?php
+								$id = get_the_id();
+							?>
+								<h2 id=<?php echo $id; ?>>
+								<a href="<?php $id; ?> "><?php the_title(); ?></a>
+								</h2>
+							
+						</article>
+						<?php
+					}
+					
+					?><p><?php echo $term->name ?></p><?php
+				}
 
-		$taxonomy = ''
-		while ( have_posts() ) :
-			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-			
 
-		endwhile; // End of the loop.
+
+			}
+		}
 		?>
 
 	</main><!-- #main -->
